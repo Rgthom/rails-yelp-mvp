468777990d98b0a7b22c739608f34d95223fbe0f
Rick Thompson <rrgthompson@googlemail.com>
2019-11-13 16:27:59 +0000

    added in everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  get 'reviews/new'
+  get 'reviews/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root to: "controller#action"
+
+
+resources :restaurants do
+  resources :reviews, only: [ :new, :create ]
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '01234 750 260',
+    category:     'belgian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '01234 750 260',
+    category:     'italian'
+  },
+
+  {
+    name:         'Hoxton 100',
+    address:      '100-102 Hoxton St, Hackney, London N1 6SG',
+    phone_number:  '01234 750 260',
+    category:     'japanese'
+  },
+  {
+    name:         'Japanese Canteen',
+    address:      'Arch 255 Paradise Row, London E2 9LQ',
+    phone_number:  '01234 750 260',
+    category:     'french'
+  },
+  {
+    name:         'The Ivy',
+    address:      '1-5 West St, London WC2H 9NQ',
+    phone_number:  '01234 750 260',
+    category:     'chinese'
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
